b8e803eca2aab36f539c6f2bba86e1870e79f09d
XiufuJ <jiangxiufu522@sina.com>
2018-11-19 11:40:34 -0500

    loginandupdate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+//import Info from './Information'; 
+import Navigation from './Navigation';
+import LogIn from './LogIn.js'; 
 
-class App extends Component {
-  render() {
+
+
+class App extends React.Component {
+
+  //Constructor
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      home: true
+    };
+  }
+
+
+ 
+
+  render() {   
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+          <div>
+            <Navigation />
+          </div>
+         
+          <Route path = "/LogIn" component = { LogIn } />
+          
+        </div>
+        
+      </Router>  
     );
   }
 }
+
 
 export default App;
---
+++ src/LogIn.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './LogIn.css'
+class ModalExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeState = this.changeState.bind(this);
+    this.state = {
+      logIn : true
+    };
+  }
+
+  changeState() {
+    this.setState({
+      logIn : !this.state.logIn
+    });
+
+    if (this.state.logIn) {
+      document.getElementById("logInPart").style.setProperty('display','block');
+      document.getElementById("signUpPart").style.setProperty('display','none');
+      document.getElementById("clickToSignUp").style.setProperty('display', 'block');
+      document.getElementById("clickToLogIn").style.setProperty('display', 'none');
+    } else {
+      document.getElementById("logInPart").style.setProperty('display','none');
+      document.getElementById("signUpPart").style.setProperty('display','block');
+      document.getElementById("clickToSignUp").style.setProperty('display', 'none');
+      document.getElementById("clickToLogIn").style.setProperty('display', 'block');
+    }
+  }
+
+  render(){
+    let logInPage = (
+      <div className = "logBackground">
+        <div className = "logAndSign mx-auto shadow rounded border"> 
+          <div id = "logInPart">
+            <div className="input row">
+              <strong className = "labelLen">Username: </strong>
+              <input type="text" className="form-control inputLen" id = "logInUsername"></input>
+            </div> 
+            <div className="input row">
+              <strong className = "labelLen">Password: </strong>
+              <input type="password" className="form-control inputLen" id = "logInPassword"></input>
+            </div>
+            <a href = "#" className = "btn btn-primary btnProperty text-white font-weight-bold">Log in</a>
+          </div>
+
+          <div id = "signUpPart">
+            <div className="input row">
+              <strong className = "labelLen">Username: </strong>
+              <input type="text" className="form-control inputLen" id = "signUpUsername"></input>
+            </div> 
+            <div className="input row">
+              <strong className = "labelLen">Email: </strong>
+              <input type="test" className="form-control inputLen" id = "signUpEmail"></input>
+            </div>
+            <div className="input row">
+              <strong className = "labelLen">Password: </strong>
+              <input type="password" className="form-control inputLen" id = "signUpPassword"></input>
+            </div>
+            <div className="input row">
+              <strong className = "labelLen">Reinput Password: </strong>
+              <input type="password" className="form-control inputLen" id = "passwordTwo"></input>
+            </div>
+            <a href = "#" className = "btn btn-success btnProperty text-white font-weight-bold">Sign up</a>
+          </div>
+
+          <div className = "changePart">
+            <a id = "clickToSignUp" href = "#" onClick = {this.changeState}> Doesn't have an account? Click to sign up. </a>
+            <a id = "clickToLogIn" href = "#" onClick = {this.changeState}> Already have an account? Click to log in. </a>
+          </div>
+           
+         
+        </div>
+        
+      </div>
+    );
+
+   
+
+    return logInPage
+  }
+}
+
+export default ModalExample;
