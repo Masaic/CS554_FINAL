ff4ec8a624d243fc8728d9cc6c258d8d0156cf8c
Masaic <kmi1003@163.com>
2018-12-06 21:29:59 -0500

    update navbar code

--- src/components/LogIn_SignUp.js
+++ src/components/LogIn_SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navigation from './Navigation';
 import './LogIn_SignUp.css'
 
 class LogIn_SignUp extends React.Component {
@@ -6,14 +7,14 @@ class LogIn_SignUp extends React.Component {
     super(props);
     this.switchPage = this.switchPage.bind(this);
     this.state = {
-      logIn : true
+      logIn: true
     };
   }
-  
+
   //Set new state
   async changeState() {
     this.setState({
-      logIn : !this.state.logIn
+      logIn: !this.state.logIn
     });
   }
 
@@ -22,61 +23,66 @@ class LogIn_SignUp extends React.Component {
     await this.changeState();
 
     if (this.state.logIn) {
-      document.getElementById("logInPart").style.setProperty('display','block');
-      document.getElementById("signUpPart").style.setProperty('display','none');
+      document.getElementById("logInPart").style.setProperty('display', 'block');
+      document.getElementById("signUpPart").style.setProperty('display', 'none');
       document.getElementById("clickToChange").setAttribute('text', 'Doesn\'t have an account? Click to sign up.');
-     
+
     } else {
-      document.getElementById("logInPart").style.setProperty('display','none');
-      document.getElementById("signUpPart").style.setProperty('display','block');
+      document.getElementById("logInPart").style.setProperty('display', 'none');
+      document.getElementById("signUpPart").style.setProperty('display', 'block');
       document.getElementById("clickToChange").setAttribute('text', 'Already have an account? Click to log in.');
     }
   }
 
-  render(){
-    let logInPage = ( 
-      <div className = "logBackground">
-        <div className = "logAndSign rounded border test-align"> 
-          <div id = "logInPart">
-            <div className="input row">
-              <strong className = "labelLen">Username: </strong>
-              <input type="text" className="form-control inputLen" id = "logInUsername" />
-            </div> 
-            <div className="input row">
-              <strong className = "labelLen">Password: </strong>
-              <input type="password" className="form-control inputLen" id = "logInPassword" />
+  render() {
+    let logInPage = (
+      <div>
+        <div>
+          <Navigation handleProfileChange={this.handleProfileChange} />
+        </div>
+        <div className="logBackground">
+          <div className="logAndSign rounded border test-align">
+            <div id="logInPart">
+              <div className="input row">
+                <strong className="labelLen">Username: </strong>
+                <input type="text" className="form-control inputLen" id="logInUsername" />
+              </div>
+              <div className="input row">
+                <strong className="labelLen">Password: </strong>
+                <input type="password" className="form-control inputLen" id="logInPassword" />
+              </div>
+              <a href="javascript:void(0)" className="btn btn-primary btnProperty text-white font-weight-bold">Log in</a>
             </div>
-            <a href = "javascript:void(0)" className = "btn btn-primary btnProperty text-white font-weight-bold">Log in</a>
-          </div>
 
-          <div id = "signUpPart">
-            <div className="input row">
-              <strong className = "labelLen text-right">Username: </strong>
-              <input type="text" className="form-control inputLen" id = "signUpUsername" />
-            </div> 
-            <div className="input row">
-              <strong className = "labelLen text-right">Email: </strong>
-              <input type="test" className="form-control inputLen" id = "signUpEmail" />
+            <div id="signUpPart">
+              <div className="input row">
+                <strong className="labelLen text-right">Username: </strong>
+                <input type="text" className="form-control inputLen" id="signUpUsername" />
+              </div>
+              <div className="input row">
+                <strong className="labelLen text-right">Email: </strong>
+                <input type="test" className="form-control inputLen" id="signUpEmail" />
+              </div>
+              <div className="input row">
+                <strong className="labelLen text-right">Password: </strong>
+                <input type="password" className="form-control inputLen" id="signUpPassword" />
+              </div>
+              <div className="input row">
+                <strong className="labelLen text-right">Reinput Password: </strong>
+                <input type="password" className="form-control inputLen" id="passwordTwo" />
+              </div>
+              <a href="javascript:void(0)" className="btn btn-success btnProperty text-white font-weight-bold">Sign up</a>
             </div>
-            <div className="input row">
-              <strong className = "labelLen text-right">Password: </strong>
-              <input type="password" className="form-control inputLen" id = "signUpPassword" />
-            </div>
-            <div className="input row">
-              <strong className = "labelLen text-right">Reinput Password: </strong>
-              <input type="password" className="form-control inputLen" id = "passwordTwo" />
-            </div>
-            <a href = "javascript:void(0)" className = "btn btn-success btnProperty text-white font-weight-bold">Sign up</a>
-          </div>
 
-          <div className = "changePart">
-            <a href = "javascript:void(0)" id = "clickToChange"  onClick = {this.switchPage}> Doesn't have an account? Click to sign up. </a>
+            <div className="changePart">
+              <a href="javascript:void(0)" id="clickToChange" onClick={this.switchPage}> Doesn't have an account? Click to sign up. </a>
+            </div>
           </div>
-        </div>      
+        </div>
       </div>
     );
 
-   
+
 
     return logInPage
   }
