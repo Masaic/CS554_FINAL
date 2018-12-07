cbc018eb590b107b28f086cb9354d3ae181d37c1
Bo Li <ee06b056@users.noreply.github.com>
2018-12-07 11:35:38 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -7,16 +7,32 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 // import Navigation from './components/Navigation';
 import LogIn_SignUp from './components/LogIn_SignUp';
 
+// firebase api
+import fire from './config/Fire';
 
 
-class App extends React.Component {
+
+class App extends Component {
 
   //Constructor
   constructor(props, context) {
     super(props, context);
     this.state = {
-      home: true
+      home: true,
+      user: null
     };
+  }
+
+  // authen state monitor
+  authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({user});
+      } else {
+        this.setState({user: null});
+      }
+    })
   }
 
 
@@ -27,20 +43,6 @@ class App extends React.Component {
     return (
       <Router>
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
         <div className="App-body">
         <Switch>
           <Route path = "/LogIn_SignUp" component = { LogIn_SignUp } />
