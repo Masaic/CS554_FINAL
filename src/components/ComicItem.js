feae0e16ddec30a752575ce328b7c917c36381e0
Xiufu Jiang <xiufujiang@XiufudeMBP.home>
2018-12-10 22:24:17 -0500

    update-bav-link

--- src/components/ComicItem.js
+++ src/components/ComicItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import imgNA from '../images/imgNA.jpg';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './general.css';
 
 
@@ -27,7 +27,7 @@ class ComicItem extends Component{
                     <img className="card-img-top card-img" src = {this.state.imgSrc} alt={this.props.info.title} />
                     <div className="card-body ">
                         <h5 className="card-title card-intro">{this.props.info.title}</h5>
-                        <button to = {`/comics/detail/${this.props.info.id}`} onClick = {this.handleChange.bind(this, this.props.info.id)} className="btn btn-primary">Check details</button>
+                        <NavLink to = {`/comics/detail/${this.props.info.id}`} onClick = {this.handleChange.bind(this, this.props.info.id)} className="btn btn-primary">Check details</NavLink>
                     </div>
                 </div>
             </div>
